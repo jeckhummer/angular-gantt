6d78022e371a006ef07bd6ed375cab4f82b7bb39
Maksim <jeckhummer@gmail.com>
2016-03-15 11:40:03 +0400

    baselines started

---
+++ app/mocks/gantt-baselines-mock.js
@@ -0,0 +1,37 @@
+(function () {
+    angular.module('app-dev').value('GanttBaselinesMock', getGanttBaselinesMock());
+    function getGanttBaselinesMock() {
+        var baselines = {
+            'Saved Baseline 1': [
+                {id: 1, name: "Task 1", start: "2015-12-20", end: "2016-11-10", parentID: 0},
+                {id: 2, name: "SubTask A", start: "2015-12-20", end: "2016-04-01", parentID: 1},
+                {id: 3, name: "SubTask B", start: "2016-04-01", end: "2016-08-16", parentID: 1},
+                {id: 4, name: "SubTask C", start: "2016-08-16", end: "2016-11-10", parentID: 1},
+                {id: 5, name: "Task 2", start: "2016-05-22", end: "2016-12-02", parentID: 0},
+                { id: 6, name: "Sub Milestone A long name bla bla bla", start: "2016-05-22", end: "2016-05-22", parentID: 5},
+                {id: 7, name: "SubTask A", start: "2016-05-22", end: "2016-06-29", parentID: 5},
+                {id: 8, name: "SubTask B", start: "2016-06-29", end: "2016-11-10", parentID: 5},
+                {id: 9, name: "SubTask C", start: "2016-11-10", end: "2016-12-01", parentID: 5},
+                {id: 10, name: "Sub Milestone B", start: "2016-12-01", end: "2016-12-01", parentID: 5}
+            ],
+            'Saved Baseline 2': [
+                {id: 1, name: "Task 1", start: "2015-12-20", end: "2016-11-10", parentID: 0},
+                {id: 2, name: "SubTask A", start: "2015-12-20", end: "2016-04-01", parentID: 1},
+                {id: 3, name: "SubTask B", start: "2016-04-01", end: "2016-08-16", parentID: 1},
+                {id: 4, name: "SubTask C", start: "2016-08-16", end: "2016-11-10", parentID: 1},
+                {id: 5, name: "Task 2", start: "2016-05-22", end: "2016-12-02", parentID: 0},
+                { id: 6, name: "Sub Milestone A long name bla bla bla", start: "2016-05-22", end: "2016-05-22", parentID: 5},
+                {id: 7, name: "SubTask A", start: "2016-05-22", end: "2016-06-29", parentID: 5},
+                {id: 8, name: "SubTask B", start: "2016-06-29", end: "2016-11-10", parentID: 5},
+                {id: 9, name: "SubTask C", start: "2016-11-10", end: "2016-12-01", parentID: 5},
+                {id: 10, name: "Sub Milestone B", start: "2016-12-01", end: "2016-12-01", parentID: 5}
+            ]
+        };
+
+        var mock = {
+            baselines: baselines,
+            //saveResponse: saveResponse
+        };
+        return mock;
+    }
+})();
--- app/mocks/http-mocks.js
+++ app/mocks/http-mocks.js
@@ -1,12 +1,15 @@
 (function () {
     angular.module('app-dev').run(HttpMocks);
 
-    function HttpMocks($httpBackend, HttpService, GanttTasksMock, GanttTimelineMock) {
+    function HttpMocks($httpBackend, HttpService, GanttTasksMock, GanttTimelineMock, GanttBaselinesMock) {
         $httpBackend.whenGET(HttpService.getResourceUrl('gantt-tasks'))
             .respond(200, GanttTasksMock.tasks);
 
         $httpBackend.whenPOST(HttpService.getResourceUrl('gantt-task', 'post'))
             .respond(200, GanttTasksMock.saveResponse.success);
+
+        $httpBackend.whenGET(HttpService.getResourceUrl('gantt-baselines'))
+            .respond(200, GanttBaselinesMock.baselines);
 
         $httpBackend.whenGET(HttpService.getResourceUrl('gantt-timeline-options'))
             .respond(200, GanttTimelineMock.options);

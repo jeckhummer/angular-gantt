42179feae87caf1c4bce4bfee502be28ba937339
Maksim <jeckhummer@gmail.com>
2016-04-18 17:49:44 +0400

    prefix setter excluded from init method

--- app/app-url-locale.config.js
+++ app/app-url-locale.config.js
@@ -1,32 +1,12 @@
 'use strict';
 
 (function () {
-    angular.module('app').config(URLLocaleServiceConfig);
+    angular.module('app').config(URLLocalePrefixConfig);
 
-    function URLLocaleServiceConfig(URLLocaleServiceProvider){
-        var urlMapping = [
-            { name: 'gantt', url: 'gantt/gantt.html'},
-            { name: 'status-bar', url: 'gantt/status-bar/status-bar.html'},
-            { name: 'tabs', url: 'gantt/status-bar/tabs.html'},
-            { name: 'context-actions-bar', url: 'gantt/context-actions-bar/context-actions-bar.html'},
-            { name: 'task', url: 'gantt/tasks/task.html'},
-            { name: 'task-info', url: 'gantt/tasks/tasks-info.html'},
-            { name: 'task-context-menu', url: 'gantt/tasks/task-context-menu.html'},
+    function URLLocalePrefixConfig(URLLocaleServiceProvider){
+        //var prefix = 'jsWidgets/angular-gantt/app/';
+        var prefix = '';
 
-            { name: 'timeline.timeline', url: 'timeline/timeline/timeline.directive.html'},
-            { name: 'timeline.options', url: 'timeline/timeline-options/timeline-options.directive.html'},
-            { name: 'timeline.stripes', url: 'timeline/timeline-stripes/timeline-stripes.directive.html'},
-
-            { name: 'timeline-options', url: 'gantt/dialogs/timeline-options.html'},
-            { name: 'task-editor', url: 'gantt/dialogs/task-editor.html'},
-            { name: 'gantt-baselines-menu', url: 'gantt/dialogs/gantt-baselines-menu.html'},
-            { name: 'processing-lock', url: 'gantt/dialogs/processing-lock.html'},
-
-            { name: 'test', url: 'test/test.html'}
-        ];
-
-        var prefix = 'jsWidgets/angular-gantt/app/';
-
-        URLLocaleServiceProvider.init(urlMapping, prefix);
+        URLLocaleServiceProvider.setPrefix(prefix);
     }
 }());

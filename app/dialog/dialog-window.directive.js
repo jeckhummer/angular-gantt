85e435d07da20cca352d58bd29399faeea7f4701
Maksim <jeckhummer@gmail.com>
2016-04-18 19:00:41 +0400

    dialog module [loading order] bug fixed

--- app/dialog/dialog-window.directive.js
+++ app/dialog/dialog-window.directive.js
@@ -3,13 +3,20 @@
     angular.module('dialog')
         .directive('dialogWindow', DialogWindow);
 
-    function DialogWindow() {
+    function DialogWindow($rootScope) {
         var directive = {
             restrict: 'A',
             link: link
         };
         return directive;
 
-        function link(scope, element, attr){ }
+        function link(scope, element, attr) {
+            $rootScope.$on('dialog-background-loaded', _notify);
+            _notify();
+
+            function _notify() {
+                $rootScope.$broadcast('dialog-window-loaded', element);
+            }
+        }
     }
 })();

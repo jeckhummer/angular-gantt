1edd84fc85ad7ce6bb682e4f68eb1aa6c07f099b
Maksim <jeckhummer@gmail.com>
2016-05-05 10:51:49 +0400

    state manager class added; dictionary class added; project files restructuring;

---
+++ app/common/classes/dictionary/dictionary.js
@@ -0,0 +1,117 @@
+function Dictionary(objects, keysProvider, valueProvider){
+    var _self = this;
+    var _keysProvider = keysProvider || _defaultKeysProvider;
+    var _valueProvider = valueProvider || _defaultValueProvider;
+    var _dictionary;
+    var _chainedDictionary;
+    var _values;
+    var _keys;
+
+    _self.add = add;
+    _self.addRange = addRange;
+    _self.get = get;
+    _self.remove = remove;
+    _self.getValues = getValues;
+    _self.getKeys = getKeys;
+    _self.reset= reset;
+    _self.processRange = processRange;
+    _self.getSize = getSize;
+    _self.isEmpty = isEmpty;
+    _self.chainTo = chainTo;
+
+    _init();
+
+    function _init(){
+        reset();
+        addRange(objects);
+    }
+
+    function reset(){
+        _chainedDictionary = null;
+        _dictionary = {};
+        _values = [];
+        _keys = [];
+    }
+
+    function add(object){
+        var keys = _keysProvider(object);
+        if(!Array.isArray(keys)){
+            keys = [keys];
+        }
+        var value = _valueProvider(object);
+
+        keys.forEach(function (key) {
+            if(_dictionary[key]){
+                _dictionary[key].push(value);
+            }else{
+                _dictionary[key] = [value];
+            }
+        });
+
+        _values.push(value);
+        _keys = _keys.concat(keys);
+    }
+
+    function addRange(objects){
+        objects.forEach(function (object) {
+            add(object);
+        });
+    }
+
+    function get(key){
+        var values = _dictionary[key];
+
+        if(_chainedDictionary){
+            var keys = values.slice();
+            values = [];
+
+            keys.forEach(function (key) {
+                values = values.concat(_chainedDictionary.get(key));
+            });
+        }
+
+        return values;
+    }
+
+    function getValues(){
+        return _values;
+    }
+
+    function getKeys() {
+        return _keys;
+    }
+
+    function remove(key){
+        var object = get(key);
+        var index = _values.indexOf(object);
+
+        delete _dictionary[key];
+        _values.splice(index, 1);
+    }
+
+    function processRange(processor){
+        return _values.map(processor);
+    }
+
+    // readonly
+    function getSize(){
+        return _values.length;
+    }
+
+    // readonly
+    function isEmpty(){
+        return getSize() == 0;
+    }
+
+    function chainTo(dictionary) {
+        _chainedDictionary = dictionary;
+        return _chainedDictionary;
+    }
+
+    function _defaultKeysProvider(object){
+        return object.id;
+    }
+    function _defaultValueProvider(object){
+        return object;
+    }
+}
